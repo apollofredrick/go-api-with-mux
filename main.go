ba59264636bb1e433e7cd890c983a4c18e877d76
Fredrick Apollo <57845077+apollofredrick@users.noreply.github.com>
2023-02-05 13:58:10 +0300

    creating structures and endpoint logic

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
 
 }
@@ -22,6 +36,7 @@ func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", Firstname: "Nick", Lastname: "Pope", Address: &Address{City: "Dublin", State: "California"}})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
