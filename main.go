023b3abdb32e7f12e2ae703ab39b6d546a7ce83b
Fredrick Apollo <57845077+apollofredrick@users.noreply.github.com>
2023-02-05 19:33:01 +0300

    creating data

--- main.go
+++ main.go
@@ -36,10 +36,22 @@ func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main() {
