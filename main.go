e791d4b31b2cfcc29329cd4494a146a74ca5ffdd
Fredrick Apollo <57845077+apollofredrick@users.noreply.github.com>
2023-02-05 13:35:04 +0300

    creating api endpoints

--- main.go
+++ main.go
@@ -7,8 +7,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
+
+}
+func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	router := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":12345"))
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":12345", router))
 
 }
